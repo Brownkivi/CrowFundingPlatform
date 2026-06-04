8938fd6c16dbe11fa9e1c101017ac977d2bfe20f
hhh <1301058612@qq.com>
2026-06-04 13:50:47 +0800

    合约

--- contracts/core/CrawFundingFactory.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.28;
-
-contract CrawFundingFactory {
-  address[] public campaigns;
-  mapping(address => uint256[]) public userCampaigns;
-
-
-}
